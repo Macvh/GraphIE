e326fccc8658ff8a017c162b424b6f28359dcbb4
Macvh <275728483@qq.com>
2023-04-19 21:15:23 +0800

    firstly propose

---
+++ EntityLink.py
@@ -0,0 +1,7 @@
+
+
+# entities =[{"人物":"旅美大熊猫丫丫"},]{"人物":"旅美大熊猫丫丫"}
+# for entity in entities:
+#     for ent in doc.ents:
+#         if entity in ent.text and ent.label_ == "PER":
+#             text = text.replace(entity, ent.text)
